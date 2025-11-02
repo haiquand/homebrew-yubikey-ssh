c377e544ef8a762ceb28762c8eacc97efff0d8e8
haiquand <haiquan.d@outlook.com>
2025-11-02 22:55:15 +0000

    Change ssh-askpass-mac depends_on

--- Formula/ssh-askpass-mac.rb
+++ Formula/ssh-askpass-mac.rb
@@ -4,7 +4,8 @@ class SshAskpassMac < Formula
     url "https://github.com/haiquand/ssh-askpass-mac/archive/refs/tags/v0.1.0.tar.gz"
     sha256 "cf386b11964c5d71c2a06c9a6a8e2106b6216b7633c411a16acd6c64b2819db3"
 
-    depends_on "xcode" => :build
+    depends_on xcode: :build
+    depends_on :macos
 
     def install
         system "make"
