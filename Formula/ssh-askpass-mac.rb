2c48f82d3711f302ac0aa0aad7c0b92581ad0192
haiquand <haiquan.d@outlook.com>
2025-11-02 22:50:16 +0000

    Add ssh-askpass-mac utility for macOS

---
+++ Formula/ssh-askpass-mac.rb
@@ -0,0 +1,13 @@
+class SshAskpassMac < Formula
+    desc "A simple SSH Askpass utility for macOS, implemented with SwiftUI."
+    homepage "https://github.com/haiquand/ssh-askpass-mac"
+    url "https://github.com/haiquand/ssh-askpass-mac/archive/refs/tags/v0.1.0.tar.gz"
+    sha256 "cf386b11964c5d71c2a06c9a6a8e2106b6216b7633c411a16acd6c64b2819db3"
+
+    depends_on xcode :build
+
+    def install
+        system "make"
+        prefix.install "build/Build/Products/Release/ssh-askpass-mac.app"
+    end
+end
